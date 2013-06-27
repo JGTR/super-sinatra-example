5469e0e50c7b2ed777c3d336a5ad151517d75452
Jordan Trevino <jordan.trevino@gmail.com>
2013-06-27 17:46:36 -0400

    first commit

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module SuperSinatra
   class App < Sinatra::Base
 
     get '/' do
+      erb :super_hero
+    end
+
+    post '/team' do
+      @teamname = params[:team][:name]
+      @teammoto = params[:team][:moto]
+
+      @hero1 = params[:team][:hero][0]
+      @hero2 = params[:team][:hero][1]
+
+      erb :team
     end
 
   end
